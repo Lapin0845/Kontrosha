14bebc23ec3730221d580dc2d135728f3fe4ed97
Lapin0845 <126096668+Lapin0845@users.noreply.github.com>
2023-05-13 22:59:32 +0400

    Add files via upload

---
+++ kontrol.js
@@ -0,0 +1,36 @@
+let but =document.getElementById('but1')
+let form =document.getElementById('form1')
+
+
+form.onsubmit = f1
+
+function f1(){
+    console.log(form.elements);
+    console.log(form.elements.length);
+    let stroka=''
+    let elems = form.elements
+    for (e in elems) {
+        if (e == form.elements.length - 1) {
+            break
+        }
+        if (elems[e].name == '') {
+            continue
+        }
+        if (elems[e].value == undefined) {
+            continue
+        }
+        stroka += elems[e].name
+        stroka += '--' + elems[e].value + '\n'
+    }
+    console.log(stroka)
+    saveToPC(stroka)
+    return false
+}
+
+function saveToPC(str){
+let blob = new Blob([str], {type: "text/plain"});
+let link = document.createElement("a");
+link.setAttribute("href", URL.createObjectURL(blob));
+link.setAttribute("download", "123.txt");
+link.click();
+}
