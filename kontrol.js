307a346d8964bd6c496606e6373f3033f3c379ad
Lapin0845 <126096668+Lapin0845@users.noreply.github.com>
2023-05-15 21:34:14 +0400

    Add files via upload

--- kontrol.js
+++ kontrol.js
@@ -10,6 +10,14 @@ function f1(){
     let stroka=''
     let elems = form.elements
     for (e in elems) {
+        if (elems[e].type == 'checkbox' && elems[e].checked) {
+            stroka += elems[e].name
+            stroka += '--выбран' + '\n'
+        }
+        if (elems[e].type == 'radio' && elems[e].checked){
+            stroka += elems[e].name
+        stroka += '--' + elems[e].value + '\n'
+        }
         if (e == form.elements.length - 1) {
             break
         }
